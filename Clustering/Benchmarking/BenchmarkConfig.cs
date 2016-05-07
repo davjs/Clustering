f9a42bd819a02d814ef93823c61c4f8d044b9bb6
David Jensen <david_jensen3@hotmail.com>
2016-05-07 02:05:21 +0200

    missing file

---
+++ Clustering/Benchmarking/BenchmarkConfig.cs
@@ -0,0 +1,18 @@
+﻿using Clustering.Hierarchichal;
+using Clustering.Hierarchichal.CuttingAlgorithms;
+using Clustering.SimilarityMetrics;
+
+namespace Clustering.Benchmarking
+{
+    public class BenchmarkConfig<TClusterAlg, TCuttingAlg, TMetric> : IBenchmarkConfig
+        where TClusterAlg : ClusteringAlgorithm, new()
+        where TCuttingAlg : ICuttingAlgorithm, new()
+        where TMetric : ISimilarityMectric, new()
+    {
+        public BenchmarkConfig(string name) {Name = name;}
+        public ISimilarityMectric SimilarityMectric { get; } = new TMetric();
+        public ICuttingAlgorithm CuttingAlgorithm { get; } = new TCuttingAlg();
+        public ClusteringAlgorithm ClusteringAlgorithm { get; } = new TClusterAlg();
+        public string Name { get; }
+    }
+}
